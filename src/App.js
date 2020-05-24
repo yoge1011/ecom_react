a3fb45ec654d257f7d3032b696f3291c22089486
yoge1011 <yoge.singh1011@gmail.com>
2020-05-24 22:40:56 +0530

    shop page added item name, image and price

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
-
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
