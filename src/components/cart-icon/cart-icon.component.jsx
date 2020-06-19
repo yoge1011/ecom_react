f3eb521c1c8bd75b4a6a738b66bf56f27eab18ae
yoge1011 <yoge.singh1011@gmail.com>
2020-06-19 19:52:15 +0530

    cart item dropdown and reselect implementation

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,12 +5,13 @@ import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg'
 import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../reducer/cart/cart.action';
+import { selectCartItemsCount } from '../../reducer/cart/cart.selectors';
 
-const CartIcon  = ({toggleCartHidden}) => {
+const CartIcon  = ({toggleCartHidden, itemCount}) => { 
     return (
         <div className='cart-icon' onClick={toggleCartHidden}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{itemCount}</span>
         </div>
     )
 }
@@ -19,4 +20,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden:() => dispatch( toggleCartHidden() )
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
